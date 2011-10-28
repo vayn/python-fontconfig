a2eff6af1e6558747303bfa0b678a829a4170c8e
Vayn a.k.a. VT <vayn@vayn.de>
2011-10-28 09:04:43 +0800

    Determine building source or installing module in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # vim: set fileencoding=utf-8:
-# @Name: setup.py
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
 
@@ -9,7 +9,7 @@ ext_modules = [
             libraries=["fontconfig"])
 ]
 
-setup(
+args = dict(
   name='Font Config',
   version='0.2.0',
   author='Vayn a.k.a. VT',
@@ -18,3 +18,11 @@ setup(
   url='https://github.com/Vayn/python-fontconfig',
   ext_modules=ext_modules
 )
+
+
+if __name__ == '__main__':
+  if sys.argv[1] == 'build_ext':
+    from Cython.Distutils import build_ext
+    setup(**args)
+  else:
+    setup(**args)
