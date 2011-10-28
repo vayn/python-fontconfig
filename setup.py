9d9214de5fb22bb876ad9f6cd64adfd57111d1ad
Vayn a.k.a. VT <vayn@vayn.de>
2011-10-28 09:20:14 +0800

    Fixed building function

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ import sys
 from distutils.core import setup
 from distutils.extension import Extension
 
-ext_modules = [
-  Extension('fontconfig',
-            ['fontconfig.c'],
-            libraries=["fontconfig"])
-]
+def ext_modules(build=False):
+  if build:
+    source = 'fontconfig.pyx'
+  else:
+    source = 'fontconfig.c'
+  ext = [Extension('fontconfig', [source], libraries=["fontconfig"])]
+  return ext
 
 args = dict(
   name='Font Config',
@@ -16,13 +18,16 @@ args = dict(
   author_email='vayn@vayn.de',
   description='Python bindings for Fontconfig library',
   url='https://github.com/Vayn/python-fontconfig',
-  ext_modules=ext_modules
 )
 
 
 if __name__ == '__main__':
   if sys.argv[1] == 'build_ext':
     from Cython.Distutils import build_ext
+
+    args.update(cmdclass={'build_ext': build_ext})
+    args.update(ext_modules=ext_modules(True))
     setup(**args)
   else:
+    args.update(ext_modules=ext_modules())
     setup(**args)
