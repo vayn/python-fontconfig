4a835a9d46a2027f04c8c9f9887d72c983821ae2
Vayn a.k.a. VT <vayn@vayn.de>
2011-10-28 08:51:21 +0800

    Splited building and installation

--- build.py
+++ build.py
@@ -1,20 +1,20 @@
+#!/usr/bin/env python3
 # vim: set fileencoding=utf-8:
+# @Author: Vayn a.k.a. VT <vayn@vayn.de>
 # @Name: setup.py
+# @Date: 2011年10月24日 星期一 16时56分48秒
 from distutils.core import setup
 from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 ext_modules = [
   Extension('fontconfig',
-            ['fontconfig.c'],
+            ['fontconfig.pyx'],
             libraries=["fontconfig"])
 ]
 
 setup(
   name='Font Config',
-  version='0.2.0',
-  author='Vayn a.k.a. VT',
-  author_email='vayn@vayn.de',
-  description='Python bindings for Fontconfig library',
-  url='https://github.com/Vayn/python-fontconfig',
+  cmdclass={'build_ext': build_ext},
   ext_modules=ext_modules
 )
