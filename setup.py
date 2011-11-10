0a46f7b027aa709f485bd801158daa372774add5
Vayn a.k.a. VT <vayn@vayn.de>
2011-11-10 21:31:32 +0800

    Add cython classifier

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ args = dict(
   license='LICENSE.txt',
   description='Python bindings for Fontconfig library',
   long_description=open('README.rst').read(),
+
   classifiers = [
       'Development Status :: 4 - Beta',
       'Intended Audience :: Developers',
@@ -33,6 +34,7 @@ args = dict(
       'Programming Language :: Python :: 2.7',
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 3.2',
+      'Programming Language :: Cython',
   ]
 )
 
